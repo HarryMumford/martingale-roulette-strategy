df0f304577bcee3669a9caf46da1bc560d75c9df
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-03-30 00:49:18 +0100

    resets based on 1 win

--- roulette.rb
+++ roulette.rb
@@ -1,11 +1,11 @@
 class Game
-  attr_reader :profit, :spending, :bank, :initial_bet
+  attr_reader :profit, :loss, :winnings, :initial_bet
 
   def initialize
     @history = []
-    @spending = 0
-    @bank = 0
-    @initial_bet = 10
+    @loss = 0
+    @winnings = 0
+    @initial_bet = 1
     @turn = 0
     @profit = 0
   end
@@ -15,60 +15,61 @@ class Game
     num > 18 ? (@history << "red") : (@history << "black")
   end
 
-  def update_spending
-    @spending += @initial_bet
+  def update_loss
+    @loss += @initial_bet
   end
 
-  def update_bank
-    @bank += (@initial_bet * 2) if @history.last == "red"
+  def update_winnings
+    @winnings += (@initial_bet * 2) if @history.last == "red"
   end
 
   def print_game_outcome
-    p "after #{@turn} turns you won $#{@bank} and you spent $#{@spending}"
+    p "after #{@turn} turns you won $#{@winnings} and you loss $#{@loss}"
     p "Overall profit: $#{@profit}"
   end
 
   def print_roll_outcome
-    p "bet: #{@initial_bet} | bank: #{@bank} | spent: #{@spending}" 
+    p "turn: #{@turn} bet: #{@initial_bet} | winnings: #{@winnings} | lost: #{@loss}" 
   end
 
-  def run(silent)
+  def run(amount)
     while true do
       roll
-      update_spending
-      update_bank
-      print_roll_outcome unless silent
+      update_loss
+      update_winnings
+      print_roll_outcome if amount < initial_bet
       @initial_bet *= 2
       @turn += 1
-      @profit = @bank - @spending
+      @profit = @winnings - @loss
       break if @history.last == "red"
     end
-    print_game_outcome unless silent
+    # print_game_outcome
   end
 end
 
 class Strategy
-  attr_reader :bank, :spending
+  attr_reader :bank
 
-  def initialize
-    @bank = 1000
+  def initialize(starting_cash)
+    @bank = starting_cash
     @games_played = 0
-    @spending = 0
+    @loss = 0
   end
 
 
   def run(times, amount)
     while @games_played <= times do
       game = Game.new
-      game.run(true)
-      p "$#{amount} exceeded" if game.initial_bet > amount
-      @bank -= game.spending
-      @bank += game.bank
+      game.run(amount)
+      @bank -= game.loss
+      @bank += game.winnings
       @games_played += 1
     end
   end
 end
 
-strat = Strategy.new
-strat.run(10000000, 100000000)
-p strat.bank
+
+starting_cash = 1000
+strat = Strategy.new(starting_cash)
+strat.run(10, 10)
+p "starting with #{starting_cash} you made #{strat.bank - starting_cash}"
